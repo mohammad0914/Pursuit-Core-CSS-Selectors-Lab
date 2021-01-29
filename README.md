ec1ddf49368c2c0342d7b2c4e90b806a4d2e0cc8
Josh Goldberg <josh@codecademy.com>
2021-01-28 21:56:27 -0500

    Updated for CSS feedback

--- README.md
+++ README.md
@@ -25,62 +25,63 @@ This repo has an `about.html` file inside of it, as well as a `styles.css` file.
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>CSS Intro</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
-
     <h1>Your Name Here</h1>
     <div>
-      <p>This is a bunch of information about myself. I'm from here and there and discovered my
-      love of programming when this happend. When I'm not working I'm busy doing this and that.
-    </p>
+      <p>
+        This is a bunch of information about myself. I'm from here and there and discovered my love
+        of programming when this happened. When I'm not working I'm busy doing this and that.
+      </p>
     </div>
 
-    <img src="https://www.breakthrough-pt.com/wp-content/uploads/2014/11/female-default-profile-photo.png" alt="" >
+    <img
+      src="https://www.breakthrough-pt.com/wp-content/uploads/2014/11/female-default-profile-photo.png"
+      alt=""
+    />
 
-    <div >
-      <h3>Here are some of my Skills!</h3>
+    <div>
+      <h2>Here are some of my Skills!</h2>
+        
+      <h3>Languages</h3>
       <ul>
-        <h3>Languages</h3>
         <li>JavaScript</li>
         <li>SQL</li>
         <li>HTML5</li>
         <li>CSS3</li>
-
       </ul>
 
-      <ol>
         <h3>Libraries</h3>
+      <ol>
         <li>React</li>
         <li>PostgreSQL</li>
         <li>Node</li>
         <li>Bootstrap</li>
-
       </ol>
     </div>
 
     <div>
-      <h3>Hardest Bug So Far</h3>
-      <p>My hardest bug I ever came across was this infinite loop I couldn't escape. </p>
-      <p>I came up with a totally sick solution though by doing ... </p>
+      <h2>Hardest Bug So Far</h2>
+      <p>My hardest bug I ever came across was this infinite loop I couldn't escape.</p>
+      <p>I came up with a totally sick solution though by doing ...</p>
     </div>
 
     <h2>Contact Me</h2>
-    <div>
-     Email me at: <a href="mailto:hello@pursuit.org" target="_top">hello@pursuit.org</a>
-    </div>
+    <div>Email me at: <a href="mailto:hello@pursuit.org" target="_top">hello@pursuit.org</a></div>
 
     <ul>
-      <li> <a href="https://github.com">github link</a> </li>
-      <li> <a href="https://linkedin.com">LinkedIn link</a> </li>
-      <li> <a href="https://angellist.com">Angel list link</a> </li>
+      <li><a href="https://github.com">github link</a></li>
+      <li><a href="https://linkedin.com">LinkedIn link</a></li>
+      <li><a href="https://angellist.com">Angel list link</a></li>
+      <li><a href="https://pursuit.org">Pursuit link</a></li>
     </ul>
 
     <form action="index.html" method="post">
-      <input type="text" name="" value="">
-      <input type="submit" name="submit" value="submit">
+      <input type="text" name="" value="" />
+      <input type="submit" name="submit" value="submit" />
     </form>
   </body>
 </html>
@@ -93,12 +94,12 @@ This repo has an `about.html` file inside of it, as well as a `styles.css` file.
 ## CSS changes:
 
 - Assign a class named `important` to all header tags and make them all underlined and bold.
-- Give the h3 header with the text "Here are some of my Skills!" a unique id `skills-header` and make turn it RebeccaPurple in color.
+- Give the h2 header with the text "Here are some of my Skills!" a unique id `skills-header` and make turn it RebeccaPurple in color.
 - All links that end in .com should be the color green after the link is visited.
-- Give the unordered list of links a class name of `links-list` and make it have a 🌐 symbol as a bullet point.
+- Give the unordered list of links a class name of `links-list` and make it have a 🌐 symbol (`https://github.githubassets.com/images/icons/emoji/unicode/1f310.png`) as a bullet point.
 - In all ordered lists change only the last child element to be bold.
-- Give <a> elements with an href ending ".com" a hover effect of turning red.
-- Give all other <a> elements with an href a hover effect of turning orange.
+- Give <a> elements with an href ending ".com" a focus effect of turning red.
+- Give all other <a> elements with an href a focus effect of turning orange.
 - Center all text inside the body.
 - Give your languages unordered list an id of `langaguage-list`, and change its font to be in italics.
 - Make the second p tag in the div have the a background-color of rgba(0,0,0,.5).
--- cypress/integration/styling_spec.js
+++ cypress/integration/styling_spec.js
@@ -1,52 +1,83 @@
-describe('Styling', () => {
-    it("tags marked as important are bold and underlined", () => {
-      cy
-      .get('.important')
-      .should('have.css', 'font-weight', '700').and('have.css', 'text-decoration', 'underline solid rgb(0, 0, 255)')
-    })
-    it("h3 header tags are marked as skills-header and are RebeccaPurple", () => {
-      cy
-      .get('#skills-header')
-      .should('have.css', 'color', 'rgb(102, 51, 153)')
-    })
-    it("links ending in .com are green after visited", () => {
-      cy
-      .get('a[href$=".com"]').click()
-      .should('have.css', 'color', 'rgb(0,128,0)')
-    })
-    it("tags marked as links-list have a symbol as a bullet point", () => {
-      cy
-      .get('.links-list')
-      .should('have.css', 'list-style', 'outside url("https://github.githubassets.com/images/icons/emoji/unicode/1f310.png") disc')
-    })
-    it("the last child element in all ordered lists are bold", () => {
-      cy
-      .get('ol:last-child')
-      .should('have.css', 'font-weight', '700')
-    })
-    it("elements with an href ending .com have a hover effect of turning red", () => {
-      cy
-      .get('a[href$=".com"]').click()
-      .should('have.css', 'color', 'rgb(255, 0, 0)')
-    })
-    it("all other href elements without .com have a hover effect of turning orange", () => {
-      cy
-      .get('a').trigger('mouseover')
-      .should('have.css', 'color', 'rgb(255,165,0)')
-    })
-    it("all text inside the body are centered", () => {
-      cy
-      .get('body')
-      .should('have.css', 'text-align', 'center')
-    })
-    it("tags marked as language-list have italicized font", () => {
-      cy
-      .get('#language-list')
-      .should('have.css', 'font-style', 'italic')
-    })
-    it("second p tag in the div has the background color of rgba(0,0,0,.5)", () => {
-      cy
-      .get('p:nth-child(2)')
-      .should('have.css', 'background-color', 'rgba(0, 0, 0, 0.5)')
-    })
-})
+describe("Styling", () => {
+  it("header tags all contain the important class", () => {
+    cy.get("h1, h2, h3, h4, h5, h6").each((node) => expect(node).to.have.class("important"));
+  });
+
+  it("tags marked as important are bold and underlined", () => {
+    cy.get(".important")
+      .should("have.css", "font-weight", "700")
+      .and("have.css", "text-decoration", "underline solid rgb(0, 0, 255)");
+  });
+
+  it("h2 skills header has the skills-header id and is RebeccaPurple", () => {
+    cy.get("h2#skills-header")
+      .contains("Here are some of my Skills!")
+      .should("have.css", "color", "rgb(102, 51, 153)");
+  });
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/Privacy_and_the_:visited_selector
+  it.skip("links ending in .com are green after visited", () => {
+    cy.get('a[href="https://github.com"]')
+      .first()
+      .click()
+      .go("back")
+      .get('a[href="https://github.com"]')
+      .first()
+      .should("have.css", "color", "rgb(0,128,0)");
+  });
+
+  // https://developer.mozilla.org/en-US/docs/Web/CSS/Privacy_and_the_:visited_selector
+  it.skip("links not ending in .com are not green after visited", () => {
+    cy.get('a[href="https://pursuit.org"]')
+      .first()
+      .click()
+      .go("back")
+      .get('a[href="https://pursuit.org"]')
+      .first()
+      .should("not.have.css", "color", "rgb(0,128,0)");
+  });
+
+  it("tags marked as links-list have a symbol as a bullet point", () => {
+    cy.get(".links-list").should(
+      "have.css",
+      "list-style",
+      'outside url("https://github.githubassets.com/images/icons/emoji/unicode/1f310.png") disc'
+    );
+  });
+
+  it("the first child element in all ordered lists are bold", () => {
+    cy.get("ol li:first-child").should("not.have.css", "font-weight", "700");
+  });
+
+  it("the last child element in all ordered lists are bold", () => {
+    cy.get("ol li:last-child").should("have.css", "font-weight", "700");
+  });
+
+  // https://github.com/cypress-io/cypress/issues/311
+  it.skip("links ending in .com are red when focused", () => {
+    cy.get('a[href="https://github.com"]')
+      .first()
+      .focus()
+      .should("have.css", "color", "rgb(255,0,0)");
+  });
+
+  // https://github.com/cypress-io/cypress/issues/311
+  it.skip("links not ending in .com are orange when focused", () => {
+    cy.get('a[href="https://pursuit.org"]')
+      .first()
+      .focus()
+      .should("have.css", "color", "rgb(0,128,0)");
+  });
+
+  it("all text inside the body are centered", () => {
+    cy.get("body").should("have.css", "text-align", "center");
+  });
+
+  it("tags marked as language-list have italicized font", () => {
+    cy.get("#language-list").should("have.css", "font-style", "italic");
+  });
+
+  it("second p tag in the div has the background color of rgba(0,0,0,.5)", () => {
+    cy.get("p:nth-child(3)").should("have.css", "background-color", "rgba(0, 0, 0, 0.5)");
+  });
+});
